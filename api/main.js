e08b41e1d4ac577184fbeb352eedf4f6bb49b09e
Farouq Nabeeh <farouq@ems.dev>
2026-04-21 19:23:13 +0300

    debug: raw text response test

--- api/main.js
+++ api/main.js
@@ -1,12 +1,3 @@
-console.log('DEBUG: Minimal API Worker Start');
-
 module.exports = (req, res) => {
-    console.log('DEBUG: Request inside lambda:', req.url);
-    res.status(200).json({
-        message: 'Aman API: Minimal Test Success',
-        env_check: {
-            node_env: process.env.NODE_ENV,
-            db_present: !!process.env.DATABASE_URL
-        }
-    });
+    res.end('Aman API: RAW TEST');
 };
